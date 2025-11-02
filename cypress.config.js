0a14a76c5bd12f6089857f12eead8fac0f65e331
Omid-github <59606736+Omid-github@users.noreply.github.com>
2025-11-02 16:36:20 +0330

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  viewportWidth : 1080,
-  viewportHeight :1920,
+    projectId: "k7nvff", 
   e2e: {
-     baseUrl : 'https://sandbox-ebcom.mci.ir/ecm/ewano/pwa3',
-     excludeSpecPattern : ['**/1-getting-started , **/2-advanced-examples'],
-     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Implement node event listeners here
     },
-    supportFile: 'cypress/support/e2e.js',
+    baseUrl: 'https://sandbox-ebcom.mci.ir/ecm/ewano/pwa3',
   },
 });
