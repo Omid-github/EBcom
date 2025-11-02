dd2e783bfcad79a208681d4dc11574b0c3d5c7aa
Omid-github <59606736+Omid-github@users.noreply.github.com>
2025-11-02 17:00:31 +0330

    Update cypress.config.cjs

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,11 +1,18 @@
+// cypress.config.cjs
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-    projectId: "k7nvff", 
   e2e: {
-    setupNodeEvents(on, config) {
-      // Implement node event listeners here
+    baseUrl: 'https://sandbox-ebcom.mci.ir/ecm/ewano/pwa3', // your URL
+    specPattern: 'cypress/e2e/Test/*.cy.js',               // your test files
+    browser: 'chrome',
+    chromeWebSecurity: false,        // allows cross-origin and insecure connections
+    insecureSkipTlsVerify: true,     // skips invalid SSL/TLS certificate checks
+    requestTimeout: 10000,           // increase request timeout to 10s
+    responseTimeout: 10000,          // increase response timeout to 10s
+    retries: {
+      runMode: 2,                    // retry twice in CI if server is slow
+      openMode: 0,
     },
-    baseUrl: 'https://sandbox-ebcom.mci.ir/ecm/ewano/pwa3',
   },
 });
